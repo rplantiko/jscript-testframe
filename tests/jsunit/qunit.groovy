d1becfb24e600278b11513e63c16624b49a605c7
unknown <Ruediger.Plantiko@mgb.ch>
2016-03-07 15:51:13 +0100

    Added qUnit test (for UI5)

---
+++ tests/jsunit/qunit.groovy
@@ -0,0 +1,53 @@
+// SeleniumHelper.groovy befindet sich in C:\Users\rplantik\.groovy\classes
+
+import org.openqa.selenium.*;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+//import static org.junit.Assert.*;
+//import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(Parameterized.class)
+public class QUnit {
+
+  private static final String UI5 = "/sap/bc/ui5_ui5/sap/"
+
+  @Delegate private SeleniumHelper selenium;
+
+  @Parameter
+  public String testfile;
+
+  @Parameters static Collection<Object[]> data() {
+   [
+     "zrt_library/test.qunit.html"
+   ]
+  }
+
+
+
+  @Before
+  public void setUp() throws Exception {
+    selenium = new SeleniumHelper("http://dgm.migros.ch:8000");
+    //selenium.keepBrowserOnQuit = true; // Für Tests (cmd.exe mit Task Manager löschen!)
+  }
+
+  @Test
+  public void test() throws Exception {
+   println "Teste Script ${ testfile }..."
+   test_unit(testfile)
+  }
+
+  def test_unit( String file ) {
+   start( UI5 + file,"auth_dgm.html")
+   waitFor { present css:"#qunit-banner.qunit-pass" }
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    selenium.finish( )
+    }
+
+}
